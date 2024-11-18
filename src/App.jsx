ddde6f8e476fcb7ac28b1db9875acb8ee228c937
manuNamuncura <manu.namun94@gmail.com>
2024-11-17 22:15:29 -0300

    se agrego ruta para el analisis

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Home from './Components/Home';
 import Main from './Components/Main';
-import InvestmentTypes from './Components/InvestmentTypes';
+import InvestmentTypes from './Components/Analisis';
 import Bonds from './Components/Bonds';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [currentScreen, setCurrentScreen] = useState('home');
@@ -12,12 +14,23 @@ function App() {
   };
 
   return (
-    <div>
-      {currentScreen === 'home' && <Home onNavigate={navigate} />}
-      {currentScreen === 'main' && <Main onNavigate={navigate} />}
-      {currentScreen === 'investmentTypes' && <InvestmentTypes onNavigate={navigate} />}
-      {currentScreen === 'bonds' && <Bonds onNavigate={navigate} />}
-    </div>
+    // <div>
+    //   {currentScreen === 'home' && <Home onNavigate={navigate} />}
+    //   {currentScreen === 'main' && <Main onNavigate={navigate} />}
+    //   {currentScreen === 'investmentTypes' && <InvestmentTypes onNavigate={navigate} />}
+    //   {currentScreen === 'bonds' && <Bonds onNavigate={navigate} />}
+    // </div>
+    // <div>
+    //   <InvestmentTypes/>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/analisis-mercado" element={<InvestmentTypes />} />
+        {/* <Route path="/regulacion-cumplimiento" element={<RegulacionCumplimiento />} />
+        <Route path="/educacion-financiera" element={<EducacionFinanciera />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
