0680e6bda6dde74f4afb9df1480974282d40b251
manuNamuncura <manu.namun94@gmail.com>
2024-11-18 23:16:18 -0300

    se agrego la ruta

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Components/Home';
 import Main from './Components/Main';
 import InvestmentTypes from './Components/Analisis';
 import Tendencias from './Components/Tendencias';
+import Educacion from './Components/Educacion';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/analisis-mercado" element={<InvestmentTypes />} />
         <Route path="/tendencias" element={<Tendencias />} />
+        <Route path="/educacion-financiera" element={<Educacion />} />
         {/* <Route path="/regulacion-cumplimiento" element={<RegulacionCumplimiento />} />
         <Route path="/educacion-financiera" element={<EducacionFinanciera />} /> */}
       </Routes>
